e1bb0ca3bc2db5d40285c935e864d78f2ab5a1e6
Mohammed Elmallah <akhielmallah@gmail.com>
2026-09-16 18:53:08 +0200

    fix: bridge gazebo odom tf and joint states to ROS

--- src/rover_description/launch/gazebo.launch.py
+++ src/rover_description/launch/gazebo.launch.py
@@ -20,6 +20,8 @@ def generate_launch_description():
 
     xacro_file = os.path.join(pkg_dir,'urdf','rover.urdf.xacro')
 
+    sdf_file= os.path.join(pkg_dir,'worlds','warehouse.sdf')
+
     robot_description  =Command(['xacro ',xacro_file])
 
     ros_gz_sim=get_package_share_directory('ros_gz_sim')
@@ -28,7 +30,7 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(
             os.path.join(ros_gz_sim,'launch','gz_sim.launch.py')
         ),
-        launch_arguments={'gz_args': '-r empty.sdf'}.items()
+        launch_arguments={'gz_args': '-r' + sdf_file}.items()
     )
 
     bridge=IncludeLaunchDescription(

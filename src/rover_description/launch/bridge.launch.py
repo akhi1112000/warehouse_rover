2e9280fd6780c2775d953416e5cdd338bc50141d
Mohammed Elmallah <akhielmallah@gmail.com>
2026-09-16 21:55:53 +0200

    feat: add IMU sensor with gz imu system plugin

--- src/rover_description/launch/bridge.launch.py
+++ src/rover_description/launch/bridge.launch.py
@@ -13,6 +13,7 @@ def generate_launch_description():
             '/model/warehouse_rover/tf@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V',
             '/joint_states@sensor_msgs/msg/JointState[gz.msgs.Model',
             '/imu@sensor_msgs/msg/Imu[gz.msgs.IMU',
+            '/camera/image_raw@sensor_msgs/msg/Image[gz.msgs.Image',
 
         ],
         remappings=[('/model/warehouse_rover/tf', '/tf')],
